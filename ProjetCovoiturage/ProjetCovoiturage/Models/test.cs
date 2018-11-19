28dd7a2ac41d39968c2ca5fdb67297ce0a21b074
Gabriel Lanoville <gaballive@hotmail.com>
2018-11-19 17:33:38 -0500

    classe test

---
+++ ProjetCovoiturage/ProjetCovoiturage/Models/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetCovoiturage.Models
+{
+    public class test
+    {
+        public int id { get; set; }
+    }
+}
