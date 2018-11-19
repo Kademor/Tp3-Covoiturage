4b8b87e3e91547e2dc10fd4539728fea9d1d1f49
Gabriel Lanoville <gaballive@hotmail.com>
2018-11-19 17:35:08 -0500

    phil

---
+++ ProjetCovoiturage/ProjetCovoiturage/Models/phil.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetCovoiturage.Models
+{
+    public class phil
+    {
+        public int philID { get; set; }
+    }
+}
