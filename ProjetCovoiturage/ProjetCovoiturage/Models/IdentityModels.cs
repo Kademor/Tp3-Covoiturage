baa0f0a895a286c22851ece3f07cdc94ce2b1173
ShapovalovP <44822502+ShapovalovP@users.noreply.github.com>
2018-11-26 20:29:09 -0500

    Update IdentityModels.cs

--- ProjetCovoiturage/ProjetCovoiturage/Models/IdentityModels.cs
+++ ProjetCovoiturage/ProjetCovoiturage/Models/IdentityModels.cs
@@ -9,6 +9,10 @@ namespace ProjetCovoiturage.Models
     // Vous pouvez ajouter des données de profil pour l'utilisateur en ajoutant d'autres propriétés à votre classe ApplicationUser. Pour en savoir plus, consultez https://go.microsoft.com/fwlink/?LinkID=317594.
     public class ApplicationUser : IdentityUser
     {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Ville { get; set; }
+        public string Age { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Notez qu'authenticationType doit correspondre à l'élément défini dans CookieAuthenticationOptions.AuthenticationType
